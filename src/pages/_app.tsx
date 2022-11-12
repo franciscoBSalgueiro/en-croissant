8e6679dc5824e681d8016e037cc550f164cbf33f
Francisco Salgueiro <92053465+franciscoBSalgueiro@users.noreply.github.com>
2022-11-12 11:32:19 +0000

    fix SSG with mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppShell, MantineProvider } from "@mantine/core";
-import { NotificationsProvider } from '@mantine/notifications';
+import { NotificationsProvider } from "@mantine/notifications";
 import type { AppProps } from "next/app";
 import { SideBar } from "../components/Sidebar";
 
 // Chessground styles
 // import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
+import Head from "next/head";
 import "../styles/chessgroundBaseOverride.css";
 import "../styles/chessgroundColorsOverride.css";
 import "../styles/chessgroundPiecesOverride.css";
@@ -13,7 +14,14 @@ import "../styles/chessgroundPiecesOverride.css";
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <div onContextMenu={(e) => e.preventDefault()}>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
@@ -38,6 +46,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           </AppShell>
         </NotificationsProvider>
       </MantineProvider>
-    //</div>
+    </>
   );
 }
