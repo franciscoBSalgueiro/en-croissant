3d459d0270da86998bef94ad51e8c11dfa18c273
Francisco Salgueiro <fgcdbs@gmail.com>
2023-03-19 15:45:30 +0000

    new openings table

--- src/components/panels/database/DatabasePanel.tsx
+++ src/components/panels/database/DatabasePanel.tsx
@@ -1,6 +1,7 @@
-import { createStyles, Progress, ScrollArea, Table, Text } from "@mantine/core";
+import { createStyles, Progress, Text } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
 import { Square } from "chess.js";
+import { DataTable } from "mantine-datatable";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { uciToMove } from "../../../utils/chess";
 import { Opening, search_opening } from "../../../utils/db";
@@ -39,6 +40,10 @@ function DatabasePanel({
   const useOpening = useCallback(
     (referenceDatabase: string | null, fen: string) => {
       if (!referenceDatabase) return;
+      if (tree.half_moves >= 10) {
+        setOpenings([]);
+        return;
+      }
       setLoading(true);
       search_opening(referenceDatabase, fen).then((res) => {
         setLoading(false);
@@ -56,87 +61,78 @@ function DatabasePanel({
     useOpening(referenceDatabase, tree.fen);
   }, [tree, referenceDatabase]);
 
-  const rows = openings.map((opening) => {
-    const { move, white, draw, black } = opening;
-    const total = white + draw + black;
-    const whitePercent = (white / total) * 100;
-    const drawPercent = (draw / total) * 100;
-    const blackPercent = (black / total) * 100;
-    const chessMove = uciToMove(move, tree.fen);
-    if (!chessMove) return null;
-
-    return (
-      <tr
-        key={move}
-        className={classes.clickable}
-        onClick={() =>
-          makeMove({
-            from: chessMove.from as Square,
-            to: chessMove.to as Square,
-            promotion: chessMove.promotion,
-          })
-        }
-      >
-        <td width={50}>
-          <Text>{chessMove.san}</Text>
-        </td>
-        <td width={150}>
-          <Text align="right">{formatNumber(total)}</Text>
-        </td>
-        <td>
-          <Progress
-            mt="md"
-            size="xl"
-            radius="xl"
-            sections={[
-              {
-                className: classes.whiteLabel,
-                value: whitePercent,
-                color: "white",
-                label: whitePercent.toFixed(1) + "%",
-              },
-              {
-                value: drawPercent,
-                color: "gray",
-                label: drawPercent.toFixed(1) + "%",
-              },
-              {
-                value: blackPercent,
-                color: "black",
-                label: blackPercent.toFixed(1) + "%",
-              },
-            ]}
-          />
-        </td>
-      </tr>
-    );
-  });
-
   return (
-    <>
-      {loading || openings === null ? (
-        <p>Loading...</p>
-      ) : (
-        <ScrollArea h={height}>
-          <Table striped highlightOnHover>
-            <thead>
-              <tr>
-                <th>
-                  <Text align="center">Move</Text>
-                </th>
-                <th>
-                  <Text align="center">Total Games</Text>
-                </th>
-                <th>
-                  <Text align="center">White / Draw / Black</Text>
-                </th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-          </Table>
-        </ScrollArea>
-      )}
-    </>
+    <DataTable
+      withBorder
+      highlightOnHover
+      height={height}
+      records={openings}
+      fetching={loading || openings === null}
+      columns={[
+        {
+          accessor: "move",
+          width: 100,
+          render: ({ move }) => {
+            const chessMove = uciToMove(move, tree.fen);
+            if (!chessMove) return null;
+            return <Text>{chessMove.san}</Text>;
+          },
+        },
+        {
+          accessor: "total",
+          width: 100,
+          textAlignment: "right",
+          render: ({ white, draw, black }) => {
+            const total = white + draw + black;
+            return formatNumber(total);
+          },
+        },
+        {
+          accessor: "results",
+          render: ({ black, white, draw }) => {
+            const total = white + draw + black;
+            const whitePercent = (white / total) * 100;
+            const drawPercent = (draw / total) * 100;
+            const blackPercent = (black / total) * 100;
+            return (
+              <Progress
+                mt="md"
+                size="xl"
+                radius="xl"
+                sections={[
+                  {
+                    className: classes.whiteLabel,
+                    value: whitePercent,
+                    color: "white",
+                    label: whitePercent.toFixed(1) + "%",
+                  },
+                  {
+                    value: drawPercent,
+                    color: "gray",
+                    label: drawPercent.toFixed(1) + "%",
+                  },
+                  {
+                    value: blackPercent,
+                    color: "black",
+                    label: blackPercent.toFixed(1) + "%",
+                  },
+                ]}
+              />
+            );
+          },
+        },
+      ]}
+      noRecordsText="No openings found"
+      onRowClick={({ move }) => {
+        const chessMove = uciToMove(move, tree.fen);
+        if (!chessMove) return;
+        makeMove({
+          from: chessMove.from as Square,
+          to: chessMove.to as Square,
+          promotion: chessMove.promotion,
+        });
+      }}
+    />
   );
 }
 
