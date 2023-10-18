b5444654a3d86a42aef512afbba5c709b1b8f566
sleeplessghost <sleeplessghost@live.com>
2023-10-19 07:27:07 +1030

    tidying

--- src/components/common/ToggleButtonGroup.tsx
+++ src/components/common/ToggleButtonGroup.tsx
@@ -3,13 +3,32 @@ import { ReactNode } from "react";
 
 interface ToggleButtonGroupProps<T> {
     label: string;
-    options: { content: ReactNode, name: string, value: T, isToggled: boolean }[];
+    options: ToggleButtonGroupOption<T>[];
     includeTooltips?: boolean;
     minButtonWidth?: string;
     toggleOption(option: T): void;
 }
 
+export interface ToggleButtonGroupOption<T> {
+    content: ReactNode,
+    name: string,
+    value: T,
+    isToggled: boolean
+}
+
 function ToggleButtonGroup<T>(props: ToggleButtonGroupProps<T>) {
+
+    const getButton = (option: ToggleButtonGroupOption<T>) => (
+        <Button key={option.name}
+            variant={option.isToggled ? "filled" : "default"}
+            radius={0}
+            onClick={() => props.toggleOption(option.value)}
+            miw={props.minButtonWidth}
+        >
+            {option.content}
+        </Button>
+    );
+
     return (
         //this could use <InputWrapper> for the label instead if updating mantine to 7
         <Stack justify="flex-start" spacing={0}>
@@ -19,27 +38,11 @@ function ToggleButtonGroup<T>(props: ToggleButtonGroupProps<T>) {
                     if (props.includeTooltips) {
                         return (
                             <Tooltip label={option.name} key={option.name}>
-                                <Button
-                                    variant={option.isToggled ? "filled" : "default"}
-                                    radius={0}
-                                    onClick={() => props.toggleOption(option.value)}
-                                    miw={props.minButtonWidth}
-                                >
-                                    {option.content}
-                                </Button>
+                                {getButton(option)}
                             </Tooltip>
                         );
                     } else {
-                        return (
-                            <Button key={option.name}
-                                variant={option.isToggled ? "filled" : "default"}
-                                radius={0}
-                                onClick={() => props.toggleOption(option.value)}
-                                miw={props.minButtonWidth}
-                            >
-                                {option.content}
-                            </Button>
-                        );
+                        return (getButton(option));
                     }
                 })}
             </Group>
