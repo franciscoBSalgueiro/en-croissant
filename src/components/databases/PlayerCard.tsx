0794054f52e2dec7eef268ace68680e1052cd79a
Francisco Salgueiro <fgcdbs@gmail.com>
2024-02-04 18:24:40 +0000

    fix lint error

--- src/components/databases/PlayerCard.tsx
+++ src/components/databases/PlayerCard.tsx
@@ -3,8 +3,8 @@ import { Player, PlayerGameInfo } from "@/utils/db";
 import { unwrap } from "@/utils/invoke";
 import { Center, Group, Loader, Paper, Stack, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
-import PersonalPlayerCard from "../home/PersonalCard";
 import useSWRImmutable from "swr/immutable";
+import PersonalPlayerCard from "../home/PersonalCard";
 
 function PlayerCard({ player, file }: { player: Player; file: string }) {
   const { data: info, isLoading } = useSWRImmutable(
