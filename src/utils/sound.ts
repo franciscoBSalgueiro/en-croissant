b6cd5221dfa4fe5a166559df3a9b2ab5bd7cad98
Francisco Salgueiro <fgcdbs@gmail.com>
2024-04-12 11:27:45 +0100

    limit frequency of sounds playing

--- src/utils/sound.ts
+++ src/utils/sound.ts
@@ -1,7 +1,16 @@
 import { soundCollectionAtom, soundVolumeAtom } from "@/state/atoms";
 import { getDefaultStore } from "jotai";
 
+let lastTime = 0;
+
 export function playSound(capture: boolean, check: boolean) {
+  // only play at most 1 sound every 75ms
+  const now = Date.now();
+  if (now - lastTime < 75) {
+    return;
+  }
+  lastTime = now;
+
   const store = getDefaultStore();
   const collection = store.get(soundCollectionAtom);
   const volume = store.get(soundVolumeAtom);
