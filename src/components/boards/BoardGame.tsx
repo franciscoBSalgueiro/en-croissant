7f4ebbd2b9c9808bb18756bb03f82ce08c1b28df
Ari Mourao <amlima@gmail.com>
2024-11-10 15:05:30 -0300

    fixes black incorrectly starting with increment time

--- src/components/boards/BoardGame.tsx
+++ src/components/boards/BoardGame.tsx
@@ -569,7 +569,13 @@ function BoardGame() {
       }
       if (pos?.turn === "white" && blackTime !== null) {
         setBlackTime(
-          (prev) => prev! + (players.black.timeControl?.increment ?? 0),
+          (prev) => {
+            if (pos?.fullmoves === 1) {
+              return prev!;
+            }
+
+            return prev! + (players.black.timeControl?.increment ?? 0);
+          }
         );
       }
       setIntervalId(intervalId);
