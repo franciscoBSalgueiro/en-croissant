e99af1f437775a73351bfc6d2d318c687399d0fa
Francisco Salgueiro <fbsal@proton.me>
2026-01-09 20:26:03 +0000

    add secondary icon to top bar maximize

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Center, Group, Image, Menu, Text } from "@mantine/core";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+import { useEffect, useState } from "react";
 import * as classes from "./TopBar.css";
 
 import { useColorScheme } from "@mantine/hooks";
@@ -35,6 +36,21 @@ function IconMaximize() {
   );
 }
 
+function IconSquare() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <title>Restore</title>
+      <path d="M2 4.5C2 3.11929 3.11929 2 4.5 2H11.5C12.8807 2 14 3.11929 14 4.5V11.5C14 12.8807 12.8807 14 11.5 14H4.5C3.11929 14 2 12.8807 2 11.5V4.5ZM4.5 3C3.67157 3 3 3.67157 3 4.5V11.5C3 12.3284 3.67157 13 4.5 13H11.5C12.3284 13 13 12.3284 13 11.5V4.5C13 3.67157 12.3284 3 11.5 3H4.5Z" />
+    </svg>
+  );
+}
+
 function IconX() {
   return (
     <svg
@@ -63,6 +79,24 @@ type MenuGroup = {
 
 function TopBar({ menuActions }: { menuActions: MenuGroup[] }) {
   const colorScheme = useColorScheme();
+  const [isMaximized, setIsMaximized] = useState(false);
+
+  useEffect(() => {
+    const checkMaximized = async () => {
+      const maximized = await appWindow.isMaximized();
+      setIsMaximized(maximized);
+    };
+
+    checkMaximized();
+
+    const unlisten = appWindow.onResized(() => {
+      checkMaximized();
+    });
+
+    return () => {
+      unlisten.then((fn) => fn());
+    };
+  }, []);
 
   return (
     <>
@@ -143,7 +177,7 @@ function TopBar({ menuActions }: { menuActions: MenuGroup[] }) {
               onClick={() => appWindow.toggleMaximize()}
               className={classes.icon}
             >
-              <IconMaximize />
+              {isMaximized ? <IconMaximize /> : <IconSquare />}
             </Center>
             <Center
               h="2.25rem"
