8f85fef3c0bacd0196d0e4fc03aa49292d9cca26
Francisco Salgueiro <fgcdbs@gmail.com>
2026-02-20 18:39:48 +0000

    cache transpositions

--- src/components/common/CompleteMoveCell.tsx
+++ src/components/common/CompleteMoveCell.tsx
@@ -22,20 +22,39 @@ import { type TreeNode, treeIterator } from "@/utils/treeReducer";
 import MoveCell from "./MoveCell";
 import { TreeStateContext } from "./TreeStateContext";
 
+const transpositionCache = new WeakMap<TreeNode, Map<string, number[][]>>();
+
+function getTranspositionMap(root: TreeNode) {
+  if (transpositionCache.has(root)) {
+    return transpositionCache.get(root)!;
+  }
+
+  const map = new Map<string, number[][]>();
+  const iterator = treeIterator(root);
+
+  for (const item of iterator) {
+    const strippedFen = stripClock(item.node.fen);
+    if (!map.has(strippedFen)) {
+      map.set(strippedFen, []);
+    }
+    map.get(strippedFen)!.push(item.position);
+  }
+
+  transpositionCache.set(root, map);
+  return map;
+}
+
 function getTranspositions(fen: string, position: number[], root: TreeNode) {
   if (position.length === 0 || position.every((v) => v === 0)) return [];
-  const transpositions: number[][] = [];
+
+  const map = getTranspositionMap(root);
   const strippedFen = stripClock(fen);
-  const iterator = treeIterator(root);
-  for (const item of iterator) {
-    if (hasMorePriority(position, item.position)) {
-      continue;
-    }
-    if (stripClock(item.node.fen) === strippedFen) {
-      transpositions.push(item.position);
-    }
-  }
-  return transpositions;
+
+  const matchingPositions = map.get(strippedFen) || [];
+
+  return matchingPositions.filter(
+    (targetPosition) => !hasMorePriority(position, targetPosition),
+  );
 }
 
 function CompleteMoveCell({
