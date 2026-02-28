dce8fadad656bf41843abef2bd0a8a62baea293c
Francisco Salgueiro <fgcdbs@gmail.com>
2026-02-28 19:04:22 +0000

    format

--- src/components/tabs/ImportModal.tsx
+++ src/components/tabs/ImportModal.tsx
@@ -139,8 +139,10 @@ export default function ImportModal({
         }
         pgn = res;
       } else if (link.includes("lichess")) {
-        const excludedPathParts = ['game', 'export', 'white', 'black'];
-        const gameId = new URL(link).pathname.split('/').find(x => x && !excludedPathParts.includes(x))
+        const excludedPathParts = ["game", "export", "white", "black"];
+        const gameId = new URL(link).pathname
+          .split("/")
+          .find((x) => x && !excludedPathParts.includes(x));
         if (!gameId) {
           setLoading(false);
           return;
